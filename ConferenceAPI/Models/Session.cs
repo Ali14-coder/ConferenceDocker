a1baf62389200f99b0e4896504033e95ca46ede7
Aliziwe Qeqe <st10382076@vcconnect.edu.za>
2025-04-08 10:05:44 +0200

    Add project files.

---
+++ ConferenceAPI/Models/Session.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConferenceAPI.Models
+{
+    public class Session
+    {
+        [Key]
+
+        public int SessionID { get; set; }
+
+        
+        [Required]
+        [StringLength(100)] //Maximum 100 characters for SessionTitle
+
+        public string SessionTitle { get; set; }
+
+        [Required]
+       
+
+        public DateTime SessionDateTime { get; set; }
+
+        [StringLength(100)]
+
+
+        public string Speaker { get; set; }
+
+        //navigation property for related Registrations
+        public ICollection<Registration> Registrations { get; set; }
+    }
+}
