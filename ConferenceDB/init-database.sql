89e3213025c57dbc3ed54ebbe99532026953ee72
Aliziwe Qeqe <st10382076@vcconnect.edu.za>
2025-04-10 09:04:55 +0200

    Second commit

--- ConferenceDB/init-database.sql
+++ ConferenceDB/init-database.sql
@@ -4,7 +4,7 @@ GO
 -- Create the Conference database only if it does not already exist
 IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'Conference')
 BEGIN
-	CREATE DATABASE Conference
+    CREATE DATABASE Conference;
 END
 GO
 
@@ -14,56 +14,51 @@ GO
 
 -- Create Attendees table
 CREATE TABLE Attendees (
-AttendeeID INT PRIMARY KEY IDENTITY(1,1),
-FirstName VARCHAR(50) NOT NULL,
-LastName VARCHAR(50) NOT NULL,
-Emaail VARCHAR(100) NOT NULL
+    AttendeeID INT PRIMARY KEY IDENTITY(1,1),
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) NOT NULL
 );
 GO
-
 -- Create Sessions table
 CREATE TABLE Sessions (
-SessionID INT PRIMARY KEY IDENTITY (101,1),
-SessionTitle VARCHAR(100) NOT NULL,
-SessionDateTime DATETIME NOT NULL,
-Speaker VARCHAR(50)
+    SessionID INT PRIMARY KEY IDENTITY(101,1),
+    SessionTitle VARCHAR(100) NOT NULL,
+    SessionDateTime DATETIME NOT NULL,
+    Speaker VARCHAR(50)
 );
 GO
 
--- Create Regsitrations table
-CREATE TABLE Registrations(
-RegistrationID INT PRIMARY KEY IDENTITY (1,1),
-AttendeeID INT NOT NULL,
-SessionID INT NOT NULL,
-RegistrationDate DATE NOT NULL,
-FOREIGN KEY (AttendeeID) REFERENCES Attendees(AttendeesID),
-FOREIGN KEY (SessionID) REFERENCES Sessions(SessionID),
+-- Create Registrations table
+CREATE TABLE Registrations (
+    RegistrationID INT PRIMARY KEY IDENTITY(1,1),
+    AttendeeID INT NOT NULL,
+    SessionID INT NOT NULL,
+    RegistrationDate DATE NOT NULL,
+    FOREIGN KEY (AttendeeID) REFERENCES Attendees(AttendeeID),
+    FOREIGN KEY (SessionID) REFERENCES Sessions(SessionID)
 );
 GO
 
 -- Insert data into Attendees table
-INSERT INTO Attendees(FirstName, LastName, Email)
-VALUES
+INSERT INTO Attendees (FirstName, LastName, Email)
+VALUES 
 ('John', 'Doe', 'john.doe@example.com'),
 ('Jane', 'Smith', 'jane.smith@example.com'),
-('Emily', 'Jones', 'emily.jones@example.com')
-;
+('Emily', 'Jones', 'emily.jones@example.com');
 GO
 
 -- Insert data into Sessions table
 INSERT INTO Sessions (SessionTitle, SessionDateTime, Speaker)
-VALUES
+VALUES 
 ('Introduction to C#', '2025-04-01 09:00:00', 'Dr. Alan Turing'),
-('Advanced SQL Queries', '2025-04-01 11:00:00', 'Prof.Grace'),
-('IoT: The Future of Tech', '2025-04-01 14:00:00', 'Mr. Elon Mask')
-;
+('Advanced SQL Queries', '2025-04-01 11:00:00', 'Prof. Grace Hopper'),
+('IoT: The Future of Tech', '2025-04-01 14:00:00', 'Mr. Elon Mask');
 GO
-
--- Insert data into Registration table
+-- Insert data into Registrations table
 INSERT INTO Registrations (AttendeeID, SessionID, RegistrationDate)
-VALUES
+VALUES 
 (1, 101, '2025-03-15'),
 (2, 102, '2025-03-20'),
-(3, 103, '2025-03-25')
-;
-GO
+(3, 103, '2025-03-25');
+GO 
